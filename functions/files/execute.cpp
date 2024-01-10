72a0bd9da7f0e706ef30cb07c478abb17253ecf8
Mondus <glanshammar70@gmail.com>
2024-01-10 13:07:49 +0100

    Added file execution function.

--- functions/files/execute.cpp
+++ functions/files/execute.cpp
@@ -1,2 +1,9 @@
 #include "execute.hpp"
-#include ""
+
+void ExecuteFile(const vector<string>& args, const vector<string>& options) {
+    const string& filename = args[0];
+    if(options[0] == "-help") {
+        cout << "Usage: Yo mama!" << endl;
+    }
+    cout << "Executing file: " << filename << endl;
+}
--- functions/files/execute.hpp
+++ functions/files/execute.hpp
@@ -1,6 +1,11 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
 
+#include "boost/process.hpp"
+#include <boost/filesystem.hpp>
+#include <boost/asio.hpp>
+#include "../../global.hpp"
 
+void ExecuteFile(const vector<string>& args, const vector<string>& options);
 
 #endif //EXECUTE_HPP
