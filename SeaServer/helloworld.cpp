12ed5f670e7c65697ae0e0d64cf8b627820327ed
Mondus <glanshammar70@gmail.com>
2024-02-02 16:27:42 +0100

    Changed the syntax after the ClientPrint() change.

--- SeaServer/helloworld.cpp
+++ SeaServer/helloworld.cpp
@@ -3,15 +3,15 @@
 
 // Function to handle the "HELLO" command
 void HelloWorld(const SOCKET clientSocket) {
-    SendResponse(clientSocket, "Hello from server!");
+    ClientPrint(clientSocket, "Hello from server!");
 }
 
 // Function to handle the "CUSTOM_COMMAND" command
 void CustomCommand(const SOCKET clientSocket) {
-    SendResponse(clientSocket, "Custom command executed!");
+    ClientPrint(clientSocket, "Custom command executed!");
 }
 
 // Function to handle the "ANOTHER_COMMAND" command
 void AnotherCommand(const SOCKET clientSocket) {
-    SendResponse(clientSocket, "Another command executed!");
+    ClientPrint(clientSocket, "Another command executed!");
 }
