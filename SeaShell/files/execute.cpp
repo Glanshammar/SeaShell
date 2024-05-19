bc89108fa1ae2e4d891682e7cc55236a71b67e03
Andreas Johansson <glanshammar70@gmail.com>
2024-05-19 19:48:55 +0200

    Removed Boost code for now.

--- SeaShell/files/execute.cpp
+++ SeaShell/files/execute.cpp
@@ -1,9 +1,6 @@
 #include "execute.hpp"
 
-void ExecuteFile(const vector<string>& args, const vector<string>& options) {
-    const string& filename = args[0];
-    if(options[0] == "-help") {
-        cout << "Usage: Yo mama!" << endl;
-    }
-    cout << "Executing file: " << filename << endl;
+void ExecuteFile(Arguments args, Options options) {
+    const string& path = args[0];
+    system(path.c_str());
 }
--- SeaShell/files/execute.hpp
+++ SeaShell/files/execute.hpp
@@ -1,11 +1,10 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
 
-#include "boost/process.hpp"
-#include <boost/filesystem.hpp>
-#include <boost/asio.hpp>
+
 #include "../../global.hpp"
 
-void ExecuteFile(const vector<string>& args, const vector<string>& options);
+
+void ExecuteFile(Arguments args, Options options);
 
 #endif //EXECUTE_HPP
