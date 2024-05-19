9af6562bd4002c6a9c91a87df5ff3c59fd2c4a14
Andreas Johansson <glanshammar70@gmail.com>
2024-05-19 20:05:25 +0200

    Added basic Python support.

---
+++ SeaShell/files/python.cpp
@@ -0,0 +1,36 @@
+#include "python.hpp"
+
+#define PY_SSIZE_T_CLEAN
+
+std::map<string, string> scriptMap = {
+    {"test", "C:/Users/Mondus/Documents/script.py"},
+};
+
+
+void RunPythonScript(Arguments args, Options options) {
+    wchar_t* program = Py_DecodeLocale("SeaShell", nullptr);
+
+    if(program == nullptr) {
+        cout << "Fatal error: cannot decode script path." << endl;
+        exit(1);
+    }
+
+    Py_SetProgramName(program);
+    Py_Initialize();
+
+    std::string pyPath;
+    if (scriptMap.find(args[0]) != scriptMap.end()) {
+        pyPath = scriptMap[args[0]];
+    } else {
+        pyPath = args[0];
+    }
+
+    string commandPath = "exec(open('" + pyPath + "').read())";
+    PyRun_SimpleString(commandPath.c_str());
+
+    if(Py_FinalizeEx() < 0) {
+        exit(120);
+    }
+
+    PyMem_RawFree(program);
+}
---
+++ SeaShell/files/python.hpp
@@ -0,0 +1,10 @@
+#ifndef PYTHON_HPP
+#define PYTHON_HPP
+
+#include "Python.h"
+#include "../../global.hpp"
+
+void RunPythonScript(Arguments args, Options options);
+
+
+#endif //PYTHON_HPP
