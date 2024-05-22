4daefb1cfb109e9b7b213471d046a75eee92df38
Andreas Johansson <glanshammar70@gmail.com>
2024-05-22 13:40:16 +0200

    Removed test files.

--- SeaServer/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include "../global.hpp"
-#include "functions.hpp"
-
-
-void HelloWorld(const SOCKET clientSocket) {
-    SendClient(clientSocket, "Hello from server!");
-    SendClient(clientSocket, "Hello from server!");
-    SendClient(clientSocket, "Hello from server!");
-}
