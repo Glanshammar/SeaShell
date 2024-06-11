9a0631a80c9df2747f5f551638a2a408eb64e75b
Andreas Johansson <glanshammar70@gmail.com>
2024-06-11 17:35:37 +0200

    Small reformatting change.

--- SeaShell/global.cpp
+++ SeaShell/global.cpp
@@ -3,8 +3,8 @@
 auto ptr = UniquePtr<int>(5);
 
 void UniquePointerTest() {
-    cout << *ptr << endl;
+    cout << *ptr << std::endl;
 
     auto ptr2 = std::move(ptr);
-    cout << *ptr2 << endl;
+    cout << *ptr2 << std::endl;
 }
