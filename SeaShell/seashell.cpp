2e221c84ce084bc3159cf909985ae649ef213c45
Andreas Johansson <glanshammar70@gmail.com>
2024-12-07 19:30:20 +0100

    Moved commands to separate header

--- SeaShell/seashell.cpp
+++ SeaShell/seashell.cpp
@@ -1,27 +1,8 @@
 #include <functional>
 // #include <VApplication>
-#include "global.hpp"
-#include "functions.hpp"
-#include "networking/sql.hpp"
-
-std::map<std::string, std::function<void(const std::vector<std::string>& args, const std::vector<std::string>& options)>> functionMap = {
-        {"help", PrintHelp},
-        {"exec", ExecuteFile},
-        {"py", RunPythonScript},
-        {"cd", ChangeDirectory},
-        {"ls", ListDirectoryContents},
-        {"mkdir", CreateFolder},
-        {"touch", AddFile},
-        {"rm", RemoveFile},
-        {"rmdir", RemoveFolder},
-        {"mv", FileMove},
-        {"cp", FileCopy},
-        {"ip", ListInterfaces},
-        {"zip", ZIP},
-        {"unzip", UnZIP},
-        {"find", FindFiles},
-};
-
+#include <global.hpp>
+#include <files/filesystem.hpp>
+#include <commands.hpp>
 
 int main() {
     // VApplication::setIcon("C:/Users/Mondus/Pictures/Programs/mandala.ico");
