b259c78100012e5f3f30dc856db457e4d599912e
Andreas Johansson <glanshammar70@gmail.com>
2025-05-09 10:35:54 +0200

    Updated the CLI experience

--- SeaShell/seashell.cpp
+++ SeaShell/seashell.cpp
@@ -1,64 +1,71 @@
-#include <functional>
-// #include <VApplication>
-#include <global.hpp>
-#include <files/filesystem.hpp>
-#include <commands.hpp>
+#include "commands/registry.hpp"
+#include "commands/help_command.hpp"
+#include "utils.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> splitInput(const string& input) {
+    vector<string> tokens;
+    stringstream ss(input);
+    string token;
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+vector<string> extractOptions(vector<string>& tokens) {
+    vector<string> options;
+    for (auto it = tokens.begin(); it != tokens.end();) {
+        if ((*it)[0] == '-') {
+            options.push_back(*it);
+            it = tokens.erase(it);
+        } else {
+            ++it;
+        }
+    }
+    return options;
+}
 
 int main() {
-    // VApplication::setIcon("C:/Users/Mondus/Pictures/Programs/mandala.ico");
-    // VApplication::setTitleBar("SeaShell");
-
+    auto& registry = commands::CommandRegistry::getInstance();
+    
+    // Register all commands
+    registry.registerCommand(std::make_unique<commands::HelpCommand>());
+    // TODO: Register other commands here
+    
     string input;
-    ChangeDirectory({"home"}, {});
-
     while (true) {
-        cout << CurrentDirectory << " >> " << std::flush;
+        cout << getCurrentDirectory() << " >> " << flush;
 
-        std::getline(std::cin, input);
-
-        if (input.empty()) {
-            continue;
-        } else if(input == "exit" || input == "quit" || input == "q" || input == "e") {
+        getline(cin, input);
+        if (input.empty()) continue;
+        
+        if (input == "exit" || input == "quit" || input == "q" || input == "e") {
             break;
-        } else if (input == "clear" || input == "cls") {
-            system("cls");
+        }
+        
+        if (input == "clear" || input == "cls") {
+            system("clear");  // Use clear for Linux/macOS
             continue;
         }
 
-        vector<string> tokens;
-        size_t pos;
-        while ((pos = input.find(' ')) != string::npos) {
-            string token = input.substr(0, pos);
-            if (!token.empty()) {
-                tokens.push_back(token);
-            }
-            input.erase(0, pos + 1);
-        }
-        if (!input.empty()) {
-            tokens.push_back(input);
-        }
-
+        auto tokens = splitInput(input);
         if (tokens.empty()) {
-            cout << "Invalid input. Try again." << std::endl;
+            cout << "Invalid input. Try again." << endl;
             continue;
         }
 
         string command = tokens[0];
         tokens.erase(tokens.begin());
+        auto options = extractOptions(tokens);
 
-        vector<string> options;
-        for (auto it = tokens.begin(); it != tokens.end();) {
-            if ((*it)[0] == '-') {
-                options.push_back(*it);
-                it = tokens.erase(it);
-            } else {
-                ++it;
-            }
-        }
-
-        if (functionMap.contains(command)) {
-            functionMap[command](tokens, options);
-        } else {
+        if (!registry.executeCommand(command, tokens, options)) {
+            // If command not found, try system command
             system(input.c_str());
         }
     }
