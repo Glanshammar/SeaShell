bc7851f50ef0a3d4a74421879ba57c69bea16af7
Andreas Johansson <glanshammar70@gmail.com>
2025-05-09 17:27:24 +0200

    Added header for common things

---
+++ SeaShell/common.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+// Standard library headers
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <sstream>
+
+// Common std namespace components
+using std::string;
+using std::vector;
+using std::map;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::flush;
+using std::ofstream;
+using std::ifstream;
+using std::stringstream; 
